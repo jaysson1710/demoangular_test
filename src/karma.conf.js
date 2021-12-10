6465d5f86af8c6b984cc0c68535a0b9a1f1ee89e
escobarjsa <>
2021-12-10 09:20:05 -0500

    Ajustes junit

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -33,8 +33,8 @@ module.exports = function (config) {
     browserNoActivityTimeout: 100000,
     restartOnFileChange: true,
     junitReporter: {
-      outputDir: '', // results will be saved as $outputDir/$browserName.xml
-      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputDir: 'testResults', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'Test.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
       useBrowserName: true, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
